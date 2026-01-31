e677cc266149eb3d4f643ffa63ac6ba2d14ec16c
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-01-31 16:01:43 +0200

    Implementing the UnitOfWork(UoW) Design Pattern.

---
+++ Core/Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork
+    {
+        // get IGenericRepository<TEntity,Tkey>
+
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+
+        // Save changes to database and return the number of affected records
+        Task<int> SaveChanges();
+    }
+}
---
+++ Infrastructure/Presistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿
+using Domain.Contracts;
+using Domain.Models;
+using persistence.Data;
+
+namespace persistence.Repositories
+{
+    public class UnitOfWork(StoreDbContext _dbContext) : IUnitOfWork
+    {
+        private readonly Dictionary<string, object> _repositories = new();
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+            if(_repositories.ContainsKey(typeof(TEntity).Name))
+            {
+                return (IGenericRepository<TEntity, TKey>)_repositories[typeof(TEntity).Name];
+            }
+            else
+            {
+                var repo = new GenericRepository<TEntity, TKey>(_dbContext);
+                _repositories.Add(typeof(TEntity).Name, repo);
+                return repo;
+            }
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
