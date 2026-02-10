646cc7fbb9e39a282efee5c4b5546c0cc22494e2
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-02-10 23:48:10 +0200

    Adding Basket Controller

--- Core/Service/ServiceManager.cs
+++ Core/Service/ServiceManager.cs
@@ -4,9 +4,12 @@ using ServiceAbstraction;
 
 namespace Service
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository basketRepository) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new(() => new ProductService(_unitOfWork, _mapper));
         public IProductService ProductService => _LazyProductService.Value;
+
+        private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(basketRepository, _mapper));
+        public IBasketService BasketService => _LazyBasketService.Value;
     }
 }
---
+++ Infrastructure/Presentation/Controllers/BasketController.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.DataTransferedObjects.BasketModuleDTOs;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class BasketController(IServiceManager _serviceManager):ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<BasketDTO>> GetBasket(string id)
+        {
+            var Basket = await _serviceManager.BasketService.GetBasketAsync(id);
+            return Ok(Basket);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BasketDTO>> CreateOrUpdateBasket(BasketDTO basket)
+        {
+            var Basket = await _serviceManager.BasketService.CreateOrUpdateBasketAsync(basket);
+            return Ok(Basket);
+        }
+
+        [HttpDelete("{Key}")]
+        public async Task<ActionResult<bool>> DeleteBasket(string key)
+        {
+            var Result = await _serviceManager.BasketService.DeleteBasketAsync(key);
+            return Ok(Result);
+        }
+    }
+}
