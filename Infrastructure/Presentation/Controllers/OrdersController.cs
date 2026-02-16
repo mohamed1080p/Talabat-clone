e4f405047a741b01180f51a283e50d42802144c3
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-02-17 01:04:02 +0200

    Adding more Endpoints in Orders Controller

--- Infrastructure/Presentation/Controllers/OrdersController.cs
+++ Infrastructure/Presentation/Controllers/OrdersController.cs
@@ -8,28 +8,41 @@ using System.Security.Claims;
 
 namespace Presentation.Controllers
 {
+    [Authorize]
     public class OrdersController(IServiceManager _serviceManager):ApiBaseController
     {
         // create order
-        [Authorize]
         [HttpPost]
         public async Task<ActionResult<OrderToReturnDTO>> CreateOrder(OrderDTO orderDTO)
         {
             var Email = GetEmailFromToken();
-            var Order = _serviceManager.OrderService.CreateOrder(orderDTO, Email!);
+            var Order = await _serviceManager.OrderService.CreateOrderAsync(orderDTO, Email!);
             return Ok(Order);
         }
 
         // get delivery methods
+        [AllowAnonymous]
         [HttpGet("DeliveryMethods")]
         public async Task<ActionResult<IEnumerable<DeliveryMethodDTO>>> GetDeliveryMethods()
         {
-            
+            var DeliveryMethods = await _serviceManager.OrderService.GetDeliveryMethodsAsync();
+            return Ok(DeliveryMethods);
         }
 
         // get all orders by email
-
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<OrderToReturnDTO>>> GetAllOrders()
+        {
+            var Orders = await _serviceManager.OrderService.GetAllOrdersAsync(GetEmailFromToken());
+            return Ok(Orders);
+        }
 
         // get order by id
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<OrderToReturnDTO>> GetOrderById(Guid id)
+        {
+            var Order = await _serviceManager.OrderService.GetOrderByIdAsync(id);
+            return Ok(Order);
+        }
     }
 }
